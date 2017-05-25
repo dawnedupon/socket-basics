3c456e507babc25e96c994fe768921ceae6969de
Dawn Toh <dawn.toh@gmail.com>
2017-05-25 14:49:49 +0800

    Added front-end form with basic styling

--- public/js/app.js
+++ public/js/app.js
@@ -8,3 +8,22 @@ socket.on('message', function(message) {
   console.log('New message:');
   console.log(message.text);
 });
+
+//Handles submitting of new message
+var $form = jQuery('#message-form');
+
+$form.on('submit', function(event) {
+  //preventDefault is used on a form when you don't want to refresh the entire page
+  //we want to handle form submission on our own
+  event.preventDefault();
+
+  var $message = $form.find('input[name=message]');
+
+  socket.emit('message', {
+    text: $message.val()
+  });
+
+  //Delete the value after sending it
+  $message.val('');
+
+});
